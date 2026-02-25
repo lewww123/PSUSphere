8b39094ec43debb89b861ae2ff66e6fc9b9da429
Clifford R. Caabay <ponchongcaabay@gmail.com>
2026-02-25 15:05:07 +0800

    modify allowed host -Laguisma

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7p5okbmdyggibjak+nspfsou5qyt&ua6_ia1r3rg_z51+5#y@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
