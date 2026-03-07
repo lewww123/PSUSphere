256099b9f41535268a797bd4e7e256e345de6a0b
yowbibs <202380024@psu.palawan.edu.ph>
2026-03-08 05:01:06 +0800

    modify the base.html

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7p5okbmdyggibjak+nspfsou5qyt&ua6_ia1r3rg_z51+5#y@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cliffordlewin.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['psuspherelc.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -61,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                "django.template.context_processors.debug",
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
