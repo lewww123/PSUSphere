92ca327ecb303c1528cf86f020c1ecd89530931c
Clifford R. Caabay <ponchongcaabay@gmail.com>
2026-03-08 15:33:21 +0800

    modified urls.py, views.py, base.html. added new html files for orgmember and students

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,14 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
+from studentorg.views import (HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, 
+OrganizationDeleteView, OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView,
+StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView)
 from studentorg import views  
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
+
+    path('orgmember_list', OrgMemberList.as_view(), name='orgmember-list'),
+    path('orgmember_list/add', OrgMemberCreateView.as_view(), name='orgmember-add'),
+    path('orgmember_list/<int:pk>/', OrgMemberUpdateView.as_view(), name='orgmember-update'),
+    path('orgmember_list/<pk>/delete', OrgMemberDeleteView.as_view(), name='orgmember-delete'),
+    
+    path('student_list', StudentList.as_view(), name='student-list'),
+    path('student_list/add', StudentCreateView.as_view(), name='student-add'),
+    path('student_list/<int:pk>/', StudentUpdateView.as_view(), name='student-update'),
+    path('student_list/<pk>/delete', StudentDeleteView.as_view(), name='student-delete'),
 ]
+
--- projectsite/studentorg/forms.py
+++ projectsite/studentorg/forms.py
@@ -1,9 +1,19 @@
 from django.forms import ModelForm
 from django import forms
-from .models import Organization
+from .models import Organization, OrgMember, Student
 
 class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
         fields = "__all__"
         
+class OrgMemberForm(ModelForm):
+    class Meta:
+        model = OrgMember
+        fields = "__all__"
+
+class StudentForm(ModelForm):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
